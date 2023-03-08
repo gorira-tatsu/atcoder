c5dc5aee4aa46fdec632eda26573a931a49264eb
gorira-tatsu <tatsuhiko.shigoto@gmail.com>
2023-03-08 19:50:11 +0900

    abs ABC081B

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-ss = input()
-result = []
-for i in range(len(ss)):
-    result.append(int(ss[i]))
+n = int(input())
+a = [int(i) for i in input().split()]
+possible_count = 0
 
-print(sum(result))
+while True:
+    ok = True
+    for i in range(n):
+        if a[i] % 2 == 0:
+            a[i] = a[i] // 2
+        else:
+            ok = False
+            break
+    if not ok:
+        break
+    possible_count = possible_count + 1
+print(possible_count)
