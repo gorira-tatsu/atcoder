1f322ee29c1119cabd146f29ad91671fb91ca0c7
gorira-tatsu <tatsuhiko.shigoto@gmail.com>
2023-06-30 18:57:08 +0900

    ABC085C

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-n = int(input())
-a = [int(i) for i in input().split()]
-possible_count = 0
+import sys
 
-while True:
-    ok = True
-    for i in range(n):
-        if a[i] % 2 == 0:
-            a[i] = a[i] // 2
+n_and_y = input()
+n = int(n_and_y.split()[0])
+y = int(n_and_y.split()[1])
+money_list = []
+# 10000yen 0, 5000yen 1, 1000 2
+
+for i in range(n+1):
+    for j in range(n+1):
+        k = n - (i+j)
+        if y == i*10000 + j*5000 + k*1000 and k>=0:
+            money_list = [i, j, k]
+            for l in range(3):
+                print(money_list[l], end=' ')
+            sys.exit()
         else:
-            ok = False
-            break
-    if not ok:
-        break
-    possible_count = possible_count + 1
-print(possible_count)
+            money_list = "-1 -1 -1"
+
+print(money_list)
